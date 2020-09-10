2af8265b7124c1095f8ea42db99588780430fad0
SameerDoshi <sameer.doshi@gmail.com>
2020-09-10 16:29:53 -0500

    Faking team fetch because of CORS

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       username:"",
       email:"",
       loggedin:false,
-      token: ''
+      teams:[{"teamId":101,"teamName":"Solution100","teamDescription":null,"githubURL":null,"msTeamsChannel":null,"msLabEnvironment":null,"msLabTenantName":null,"msLabAzureUsername":null,"msLabSPNAppId":null,"msLabSPNAppObjectId":null,"msLabSPNObjectId":null,"msLabSPNDisplayName":null,"msLabSPNKey":null,"active":true,"createdDate":"2020-09-07T17:09:21.1433333","createdBy":null,"modifiedDate":"0001-01-01T00:00:00","modifiedBy":null,"tblTeamSkillMatch":[],"tblTeamHackers":[]},{"teamId":102,"teamName":"Solution1","teamDescription":null,"githubURL":null,"msTeamsChannel":null,"msLabEnvironment":null,"msLabTenantName":null,"msLabAzureUsername":null,"msLabSPNAppId":null,"msLabSPNAppObjectId":null,"msLabSPNObjectId":null,"msLabSPNDisplayName":null,"msLabSPNKey":null,"active":true,"createdDate":"0001-01-01T00:00:00","createdBy":null,"modifiedDate":"0001-01-01T00:00:00","modifiedBy":null,"tblTeamSkillMatch":[],"tblTeamHackers":[]},{"teamId":103,"teamName":"Sample team1","teamDescription":"Sample desc","githubURL":null,"msTeamsChannel":null,"msLabEnvironment":null,"msLabTenantName":null,"msLabAzureUsername":null,"msLabSPNAppId":null,"msLabSPNAppObjectId":null,"msLabSPNObjectId":null,"msLabSPNDisplayName":null,"msLabSPNKey":null,"active":false,"createdDate":"0001-01-01T00:00:00","createdBy":"sadoshi@microsoft.com","modifiedDate":"0001-01-01T00:00:00","modifiedBy":null,"tblTeamSkillMatch":[],"tblTeamHackers":[]},{"teamId":104,"teamName":"Sample team2","teamDescription":"Sample desc","githubURL":null,"msTeamsChannel":null,"msLabEnvironment":null,"msLabTenantName":null,"msLabAzureUsername":null,"msLabSPNAppId":null,"msLabSPNAppObjectId":null,"msLabSPNObjectId":null,"msLabSPNDisplayName":null,"msLabSPNKey":null,"active":false,"createdDate":"0001-01-01T00:00:00","createdBy":"sadoshi@microsoft.com","modifiedDate":"0001-01-01T00:00:00","modifiedBy":null,"tblTeamSkillMatch":[],"tblTeamHackers":[]},{"teamId":105,"teamName":"team3","teamDescription":"Sample desc","githubURL":null,"msTeamsChannel":null,"msLabEnvironment":null,"msLabTenantName":null,"msLabAzureUsername":null,"msLabSPNAppId":null,"msLabSPNAppObjectId":null,"msLabSPNObjectId":null,"msLabSPNDisplayName":null,"msLabSPNKey":null,"active":false,"createdDate":"0001-01-01T00:00:00","createdBy":"sadoshi@microsoft.com","modifiedDate":"0001-01-01T00:00:00","modifiedBy":null,"tblTeamSkillMatch":[],"tblTeamHackers":[]},{"teamId":106,"teamName":"team4","teamDescription":"Sample desc","githubURL":null,"msTeamsChannel":null,"msLabEnvironment":null,"msLabTenantName":null,"msLabAzureUsername":null,"msLabSPNAppId":null,"msLabSPNAppObjectId":null,"msLabSPNObjectId":null,"msLabSPNDisplayName":null,"msLabSPNKey":null,"active":false,"createdDate":"0001-01-01T00:00:00","createdBy":"sadoshi@microsoft.com","modifiedDate":"0001-01-01T00:00:00","modifiedBy":null,"tblTeamSkillMatch":[],"tblTeamHackers":[]}]
     };
    
   }
@@ -31,7 +31,10 @@ class App extends Component {
       this.state.msalInstance.loginPopup(loginRequest)
        .then(response => {
           let id= this.state.msalInstance.getAccount(); 
-          this.setState({loggedin:true,email:id.userName,username:id.name});
+          this.setState({
+            loggedin:true,
+            email:id.userName,
+            username:id.name});
        })
        .catch(err => {
            // handle error
@@ -57,7 +60,7 @@ render() {
     <div className="App">
       {this.state.loggedin ?
       <header className="App-header">
-        <TeamsList teams={teams}/>
+        <TeamsList teams={this.state.teams}/>
       </header>
       :""}
     </div>
