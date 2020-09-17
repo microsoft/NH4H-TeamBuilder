885cd0e4c7a3e232e3965142eaed52993088e9aa
SameerDoshi <sameer.doshi@gmail.com>
2020-09-17 11:10:51 -0500

    #420 adding support for our custom security header

--- src/apis/nh4h.js
+++ src/apis/nh4h.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://nursehackapi20200906232054.azurewebsites.net/api',
-});
+    baseURL: 'https://nursehackapi20200906232054.azurewebsites.net/api',
+    headers: {
+      'ClientTeamEmbed':'caWU JvVGqXaH n9m7by',
+    }
+  });
