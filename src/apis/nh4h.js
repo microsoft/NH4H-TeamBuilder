8b9a8457e76da16616a3d15c7cf204845d0444a8
SameerDoshi <sameer.doshi@gmail.com>
2020-09-21 11:33:54 -0500

    Removing UTF char set from json requests

--- src/apis/nh4h.js
+++ src/apis/nh4h.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://nursehackapi20200906232054.azurewebsites.net/api',
     headers: {
+      common:{
+        'content-type':'application/json',
+      },
       'ClientTeamEmbed':'caWU JvVGqXaH n9m7by',
     }
   });
