69bdf670a9b5ac7ff280bf17bb65822dc04558a0
SameerDoshi <sameer.doshi@gmail.com>
2020-09-21 14:42:18 -0500

    Adding routing for users list

--- src/components/userslist.js
+++ src/components/userslist.js
@@ -1,86 +1,24 @@
 import React from 'react';
 import { Accordion, Icon } from 'semantic-ui-react'
-import TeamListItem from './teamlistitem';
+
 
 class UsersList extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      teams: [],
-      challenges:[],
-      activeIndex: 0
+      users:[]
     }
-    this.joinOrLeaveTeam=this.joinOrLeaveTeam.bind(this);
+    
   }
 
-  handleClick = (e, titleProps) => {
-    const { index } = titleProps
-    const { activeIndex } = this.state
-    const newIndex = activeIndex === index ? -1 : index
-
-    this.setState({ activeIndex: newIndex })
-  }
+   
   
-  componentDidUpdate(prevProps,prevState) {
-    if(prevProps.teams != this.props.teams){
-    let newt=this.groupBy(this.props.teams,'challengeName');
-    let newc= Object.getOwnPropertyNames(newt);
-    this.setState({
-      teams:newt,
-      challenges:newc
-    });
-  }
-  }
-
-  joinOrLeaveTeam(type,id){
-    this.props.Callback(type,id);
-  }
-
-  groupBy(array, property) {
-    var hash = {};
-    for (var i = 0; i < array.length; i++) {
-        if (!hash[array[i][property]]) hash[array[i][property]] = [];
-        hash[array[i][property]].push(array[i]);
-    }
-    return hash;
-}
-  // {this.getTeamListItems(this.state.teams[c])}
-  getChallengesList=()=>{
-    return this.state.challenges.map((c, index)=>(
-      <div key={index} >
-      <Accordion.Title
-        active={this.state.activeIndex === index}
-        index={index}
-        onClick={this.handleClick}
-        >
-        <Icon name='dropdown' />
-       {c}
-      </Accordion.Title>
-      <Accordion.Content active={this.state.activeIndex === index}>
-        <div className="ui special stackable cards">
-          {this.getTeamListItems(this.state.teams[c])}
-        </div>
-      </Accordion.Content>
-      </div>
-    ));
-  }
-  getTeamListItems=(teamlist)=>{
-  return teamlist.map( ({teamId, teamName, teamDescription,skillsWanted,tblTeamHackers,challengeName}) => ( 
-    <TeamListItem 
-      Callback={this.joinOrLeaveTeam} 
-      key={teamId} id={teamId} 
-      name={teamName} description={teamDescription}
-      isTeamMember={teamId==this.props.myteam}
-      challengeName={challengeName}      
-      skills={skillsWanted}
-      />
-  ))
-  }
   //{this.getTeamListItems(this.state.teams)}
   render() {
     return(
       <Accordion fluid styled>
-        {this.getChallengesList()}      
+       <h1>USERS LIST PLACEHOLDER!</h1>
+        {this.state.users}      
       </Accordion>
     );
   }
