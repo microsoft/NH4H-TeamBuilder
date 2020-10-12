043d658d8c18ac52269de9bcad78e44b32c65afc
Sameer Doshi <sadoshi@microsoft.com>
2020-10-12 15:01:56 -0500

    Changing base URL

--- src/apis/nh4h.js
+++ src/apis/nh4h.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://nursehackapi20200906232054.azurewebsites.net/api',
+    baseURL: 'https://nursehackteamsapi.azurewebsites.net/api',
     headers: {
       common:{
         'content-type':'application/json',
