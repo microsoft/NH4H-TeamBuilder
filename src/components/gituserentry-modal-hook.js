87f82e88ac5d915b1c0f6d3ee57738745f8d4194
Daniel Kim <dakim@microsoft.com>
2021-04-20 10:21:48 -0400

    able to get users and select

--- src/components/gituserentry-modal-hook.js
+++ src/components/gituserentry-modal-hook.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import gitapi from '../apis/gitapi';
-import { Button, Modal, Input, Dropdown, Icon } from 'semantic-ui-react'
+import { Button, Modal, Input, Dropdown, Menu, Icon, Label, Grid, Form, Divider } from 'semantic-ui-react'
 import GitHubUserEntry from './gituserentry-modal';
+import nh4h from '../apis/nh4h';
 
  
 function modalReducer (state, action) {
@@ -15,7 +16,11 @@ function modalReducer (state, action) {
   }
 }
 
+function test(event) {
+  alert('call me');
+}
 const GitHubUserEntryHook = () => {
+  const [usercount, setCount] = useState(0);
   const [username, setName] = useState(" ");
   const [ghuserlist, setUserList] = useState([]);
   const [isFetching, setFetchStatus] = useState(false);
@@ -28,15 +33,11 @@ const GitHubUserEntryHook = () => {
     }
   )
   const {dimmer, open, size} = state;
+  var count;
 
   const handleUserInput = (event, data) => {
-    console.log("event:", event)
-    setName(data.searchQuery)
-    if(username.length >=2) {
-      setFetchStatus(true);
-      setTimeout(getGitHubUser, 2000);
-    }
-    
+    setFetchStatus(true);
+    getGitHubUser(count);    
   };
 
   const letsgo = (event, data) => {
@@ -44,20 +45,40 @@ const GitHubUserEntryHook = () => {
     letsgobutton.className = "ui positive button";
   }
 
-  const getGitHubUser = () => {
-    
+  const getGitHubUser = (event, data) => {    
+    var userdropdown = document.getElementById("userdropdown");
+    //var ghuser = userdropdown.getElementsByTagName("input")[0].value;
+    //setName(userdropdown.getElementsByTagName("input")[0].value)
+    var ghuser = document.getElementById("gituserid-input").value;
     var tempghuserlist = [];
-    gitapi.get("/users?q=" + username).then((resp) => {
+    
+    gitapi.get("/users?q=" + ghuser + "&per_page=100").then((resp) => {
+      
+      setCount(resp.data.total_count);
+      console.log("totalcount:", usercount)
+      console.log(resp.data)
       resp.data.items.map(i => {
         tempghuserlist.push({ key: i.login , text: i.login , value: i.login, image: { avatar: true, src: i.avatar_url }});
         setFetchStatus(false);
         setUserList(tempghuserlist)
+        document.getElementById("displayusers").style["display"] = ""
       })       
+      
     }).catch (err => {
-
+      setFetchStatus(false);
+      console.log("i'm err:", username)
     })
   }
   
+  const saveGitUserId = () => {
+    dispatch({ type: 'CLOSE_MODAL' })
+    // nh4h.post().then((resp) => {
+    //   dispatch({ type: 'CLOSE_MODAL' })
+    // }).catch((err) => {
+
+    // });
+  };
+
   return (
     <div>
       <Modal
@@ -69,28 +90,27 @@ const GitHubUserEntryHook = () => {
         <Modal.Header>Do you have a GitHub Account?</Modal.Header>
         <Modal.Content>
           <div className="ui ">
-            <Dropdown
-              button
-              border
-              fluid
-              className='icon'
-              floating
-              labeled
-              onSearchChange={handleUserInput}
-              onChange={letsgo}
-              icon='at'
-              options={ghuserlist}
-              search
-              placeholder='Search Github User'
-              loading={isFetching}
-            />
-          {/* <Input onChange={handleUserInput} id="gituser-id" label='@' placeholder='Username' /><i aria-hidden="true" onClick={() => getGitHubUser()} className="search circular link icon"></i>*/}
+            <Form>
+              <Form.Field>
+                <Input id="gituserid-input" label='@' placeholder='Search username' action={{ onClick: () => getGitHubUser(), icon:"search" }} />
+                <Label pointing>
+                  Don't have one? It's easy! Here's <a href="https://github.com/join">how</a> :)
+                </Label> <br /><br />
+                
+              </Form.Field>
+              <Form.Field id="displayusers" style={{"display": "none"}}>
+                <Divider />
+                <Label color='teal' pointing="right">Select your username: </Label>
+                <Menu compact>
+                  <Dropdown placeholder='Select your user' onChange={letsgo} options={ghuserlist} simple item />
+                </Menu>      
+              </Form.Field>
+            </Form>
           </div>   
-          <br /><br />
-          Don't have one? It's easy! Here's <a href="https://github.com/join">how</a> :) <br /><br />
+          
         </Modal.Content>
         <Modal.Actions>
-          <Button id="letsgo" className="disabled inactive" positive onClick={() => dispatch({ type: 'CLOSE_MODAL' })}>
+          <Button id="letsgo" className="disabled inactive" positive onClick={() => { saveGitUserId(); }}>
             I'm ready!
           </Button>
         </Modal.Actions>
